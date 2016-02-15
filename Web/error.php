88d9f9a4f03910a615a66ce5de91fb149ff54a2b
Alexandr Lisovsky <oznetworkmail@gmail.com>
2016-02-15 15:16:30 +0300

    Add Web
    Add web-site

---
+++ Web/error.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ошибка - Project Blackout|OZ-Network</title>
+<meta name="description" content="Приватный сервер Project Blackout - первый в России!">
+<meta property="og:description" content="Приватный сервер Project Blackout - первый в России!">
+<meta property="og:title" content="Project Blackout|OZ-Network">
+<meta property="og:type" content="game">
+<meta property="og:url" content="http://pb.oz-network.ru/">
+<meta property="og:image" content="http://oz-network.ru/images/oz-network.png">
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="256">
+<meta property="og:image:height" content="256">
+<meta name="google-site-verification" content="JdZCABcU7pxKsfQyEPwwetR74w0lYUKpixPDedLrMlY" />
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+<div id="container">
+	<div id="bg"></div>
+	<!-- HEADER -->
+	<div id="header_bg"></div>
+	<a href="http://oz-network.ru/" title="На главную">
+		<div id="oz_logo"></div>
+	</a>
+	<a href="http://allods.oz-network.ru/" title="Легендарные Аллоды Онлайн 4.0 возвращаются!">
+		<div id="game_ao"></div>
+	</a>
+	<a href="http://pb.oz-network.ru/" title="Тактический онлайн-шутер от первого лица">
+		<div id="game_pb"></div>
+	</a>
+	<a href="http://l2.oz-network.ru/" title="Фэнтезийная онлайн игра">
+		<div id="game_l2"></div>
+	</a>
+	<a href="http://nwo.oz-network.ru/" title="Динамичные бои и сложные боссы">
+		<div id="game_nwo"></div>
+	</a>
+	<a href="http://wi.oz-network.ru/" title="Бесплатный тактический онлайн-шутер">
+		<div id="game_wi"></div>
+	</a>
+	<!-- END HEADER -->
+	<!-- MENU -->
+	<div id="menu_bg">
+	</div>
+	<a href="http://pb.oz-network.ru">
+		<div id="pb_logo"></div>
+	</a>
+	<div id="Menu">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:19px;">
+			<a href="http://forum.oz-network.ru">НОВОСТИ</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">ГАЙДЫ</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">РАНГИ</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">ФОРУМ</a></span>
+	</div>
+	<div id="Menu2">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:19px;">
+			<a href="http://pb.oz-network.ru/download.php">СКАЧАТЬ</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">МАГАЗИН</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">ОБОИ</a>&nbsp;&nbsp; &nbsp;&nbsp; 
+			<a href="http://forum.oz-network.ru">ПОМОЩЬ</a></span>
+	</div>
+	<a href="http://youtube.com/oznetgame/">
+		<div id="first_visit"></div>
+	</a>
+	<!-- END MENU -->
+	<!-- ERROR_PAGE -->
+	<div id="page_bg"></div>
+	<div id="error_image"></div>
+	<div id="error_text">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:16px;">Вы должны войти, чтобы просмотреть эту страницу.</span>
+	</div>
+	<!-- END ERROR_PAGE -->
+	<!-- LOGIN_PANEL -->
+	<div id="login_bg"></div>
+	<div id="login_icon"></div>
+	<div id="form_text">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:13px;">ФОРМА </span><span style="color:#1E90FF;font-family:Arial;font-size:13px;">ВХОДА</span>
+	</div>
+	<form id="login_account" action="http://pb.oz-network.ru/core/accounts/login_account.php" method="get">
+		<input type="text" id="login_box" name="login" value="" title="&#1051;&#1086;&#1075;&#1080;&#1085;" placeholder="&#1051;&#1086;&#1075;&#1080;&#1085;">
+		<input type="password" id="password_box" name="password" value="" title="&#1055;&#1072;&#1088;&#1086;&#1083;&#1100;" placeholder="&#1055;&#1072;&#1088;&#1086;&#1083;&#1100;">
+		<div id='login_button' onclick = 'document.getElementById(""login_account"'").submit();'>
+		</div>
+	</form>
+	<div id="repair_password_text">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:13px;">Забыли пароль ?</span><span style="color:#000000;font-family:Arial;font-size:13px;"> </span><span style="color:#1E90FF;font-family:Arial;font-size:13px;">Восстановить</span>
+	</div>
+	<a href="http://pb.oz-network.ru/reg.php">
+		<div id="reg_button"></div>
+	</a>
+	<!-- QUICK_MENU -->
+	<div id="quick_menu_bg"></div>
+	<div id="menu_icon"></div>
+	<div id="quick_menu_text">
+		<span style="color:#FFFFFF;font-family:Arial;font-size:13px;">БЫСТРОЕ </span><span style="color:#1E90FF;font-family:Arial;font-size:13px;">МЕНЮ</span>
+	</div>
+	<a href="http://forum.oz-network.ru">
+		<div id="quick_forum"></div>
+	</a>
+	<a href="http://pb.oz-network.ru/download.php">
+		<div id="quick_download"></div>
+	</a>
+	<a href="http://forum.oz-network.ru/">
+		<div id="quick_guids"></div>
+	</a>
+	<a href="http://support.oz-network.ru/">
+		<div id="quick_support"></div>
+	</a>
+	<!-- END QUCIK_MENU -->
+	<!-- FOOTER -->
+	<a href="http://pb.oz-network.ru/account.php">
+		<div id="donate"></div>
+	</a>
+	<div id="warning"></div>
+	<div id="footer_bg"></div>
+	<a href="http://oz-network.ru">
+		<div id="footer_logo"></div>
+	</a>
+	<div id="footer_info">
+		<span style="color:#DCDCDC;font-family:Arial;font-size:13px;">2013-2015 | OZ-Network - эмулятор сервера Project Blackout | Все торговые марки принадлежат их владельцам.</span>
+	</div>
+	<a href="http://zepetto.com/">
+		<div id="zepetto_logo"></div>
+	</a>
+	<!-- END FOOTER -->
+</div>
+</body>
+</html>
